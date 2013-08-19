ec1ab307206899a2a19036c70139ffb9c52ceac1
mdiamant <michael.diamant@mediacrossing.com>
2013-08-19 16:44:09 -0400

    Add canonical sbt project configuration.

---
+++ project/Build.scala
@@ -0,0 +1,43 @@
+import sbt._
+import sbtassembly.Plugin._
+import AssemblyKeys._
+import Keys._
+
+object BuildSettings {
+
+  val buildSettings210 = settings(_scalaVersion = "2.10.1", Seq("-unchecked", "-deprecation", "-feature"))
+
+  private def settings(_scalaVersion: String, _scalacOptions: Seq[String]) =
+    Defaults.defaultSettings ++
+      Seq(
+        organization := "com.mediacrossing",
+        scalaVersion := _scalaVersion,
+        scalacOptions ++= _scalacOptions,
+        testOptions in Test := Seq(Tests.Filter(s => s.endsWith("Spec"))))
+
+  lazy val customAssemblySettings = assemblySettings ++ Seq(
+    mergeStrategy in assembly <<= (mergeStrategy in assembly) {
+      (old) => {
+        //Fix to deal with some Twitter resource non-sense
+        case PathList(xs@_*) if (xs.last == "cmdline.arg.info.txt.1") => MergeStrategy.filterDistinctLines
+
+        case x => old(x)
+      }
+    }, artifact in(Compile, assembly) ~= {
+      art =>
+        art.copy(`classifier` = Some("assembly"))
+    }) ++ addArtifact(artifact in(Compile, assembly), assembly)
+
+}
+
+object TargetSegmentingBuild extends Build {
+
+  import BuildSettings._
+
+  val root = Project(
+    id = "target-segmenting",
+    base = file("."),
+    settings = buildSettings210
+  )
+}
+
