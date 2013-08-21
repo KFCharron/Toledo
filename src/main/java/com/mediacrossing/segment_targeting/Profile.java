ab9fbf58289ec52d78466e3b1395ba1ad2a061f9
KFCharron <charronkyle@gmail.com>
2013-08-21 13:07:44 -0400

    Created basic profile class

---
+++ src/main/java/com/mediacrossing/segment_targeting/Profile.java
@@ -0,0 +1,18 @@
+package com.mediacrossing.segment_targeting;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: charronkyle
+ * Date: 8/21/13
+ * Time: 12:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Profile {
+    private int id;
+    private int maxLifetimeImps;
+    private int minSessionImps;
+    private int maxSessionImps;
+    private int maxDayImps;
+    private int minMinutesPerImp;
+    private int maxPageImps;
+}
