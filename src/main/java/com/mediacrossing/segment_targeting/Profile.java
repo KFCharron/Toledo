b9616b4e24ae20f881657fb27cd52f71ae405eb9
KFCharron <charronkyle@gmail.com>
2013-08-21 13:12:08 -0400

    Added setters and getters for Profile class

--- src/main/java/com/mediacrossing/segment_targeting/Profile.java
+++ src/main/java/com/mediacrossing/segment_targeting/Profile.java
@@ -15,4 +15,62 @@ public class Profile {
     private int maxDayImps;
     private int minMinutesPerImp;
     private int maxPageImps;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMaxLifetimeImps() {
+        return maxLifetimeImps;
+    }
+
+    public void setMaxLifetimeImps(int maxLifetimeImps) {
+        this.maxLifetimeImps = maxLifetimeImps;
+    }
+
+    public int getMinSessionImps() {
+        return minSessionImps;
+    }
+
+    public void setMinSessionImps(int minSessionImps) {
+        this.minSessionImps = minSessionImps;
+    }
+
+    public int getMaxSessionImps() {
+        return maxSessionImps;
+    }
+
+    public void setMaxSessionImps(int maxSessionImps) {
+        this.maxSessionImps = maxSessionImps;
+    }
+
+    public int getMaxDayImps() {
+        return maxDayImps;
+    }
+
+    public void setMaxDayImps(int maxDayImps) {
+        this.maxDayImps = maxDayImps;
+    }
+
+    public int getMinMinutesPerImp() {
+        return minMinutesPerImp;
+    }
+
+    public void setMinMinutesPerImp(int minMinutesPerImp) {
+        this.minMinutesPerImp = minMinutesPerImp;
+    }
+
+    public int getMaxPageImps() {
+        return maxPageImps;
+    }
+
+    public void setMaxPageImps(int maxPageImps) {
+        this.maxPageImps = maxPageImps;
+    }
+
+
 }
