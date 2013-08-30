856bf9f5ffaf9d32339bec68954066f1c0fbadf7
KFCharron <charronkyle@gmail.com>
2013-08-30 14:21:17 -0400

    Added a few comments

--- src/main/java/com/mediacrossing/segmenttargeting/XlsWriter.java
+++ src/main/java/com/mediacrossing/segmenttargeting/XlsWriter.java
@@ -1,7 +1,6 @@
 package com.mediacrossing.segmenttargeting;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.*;
 
@@ -10,15 +9,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class XlsWriter {
     public void writeSegmentFileInXls (ArrayList<Campaign> campaignArrayList) {
         try {
+            //Create Workbook
             Workbook wb = new HSSFWorkbook();
             Sheet sheet = wb.createSheet("Sheet1");
-
-            CreationHelper createHelper = wb.getCreationHelper();
 
             //Header row
             Row headerRow = sheet.createRow((short) 0);
@@ -37,69 +34,74 @@ public class XlsWriter {
             headerRow.getCell(1).setCellStyle(bold);
             headerRow.getCell(2).setCellStyle(bold);
 
+            //Failed attempt to separate segments into different cells
+            /*
             //Repeat row for every campaign in list
-//            short rowCount = 1;
-//            for (Campaign campaign: campaignArrayList) {
-//                Row campaignRow = sheet.createRow(rowCount);
-//                campaignRow.createCell(0).setCellValue(campaign.getId());
-//                campaignRow.createCell(1).setCellValue(campaign.getName());
-//                List<SegmentGroupTarget> currentGroupArray = campaign.getSegmentGroupTargetList();
-//                short cellCount = 1;
-//                for (SegmentGroupTarget group: currentGroupArray) {
-//                    ArrayList<Segment> currentSegmentArray = group.getSegmentArrayList();
-//                    for(Segment segment: currentSegmentArray) {
-////                        campaignRow.createCell(cellCount++)
-////                                .setCellValue("[(" + segment.getAction() + ")" + segment.getName() + "]");
-//
-//                        System.out.println(cellCount);
-//                        if (segment.getAction().equals("exclude")) {
-//                            campaignRow.createCell(++cellCount).setCellValue("(" + segment.getAction() + ")");
-//                        }
-//
-//                        else {
-//                            campaignRow.createCell(++cellCount).setCellValue("");
-//                        }
-//                        //add left, top, bottom bold border
-//                        Font actionFont = wb.createFont();
-//                        font.setFontHeightInPoints((short)14);
-//                        font.setColor(HSSFColor.RED.index);
-//                        CellStyle topBottomLeft = wb.createCellStyle();
-//                        topBottomLeft.setFont(font);
-//                        topBottomLeft.setBorderLeft(CellStyle.BORDER_MEDIUM);
-//                        topBottomLeft.setBorderRight(CellStyle.BORDER_NONE);
-//                        topBottomLeft.setBorderTop(CellStyle.BORDER_MEDIUM);
-//                        topBottomLeft.setBorderBottom(CellStyle.BORDER_MEDIUM);
-//
-//                        campaignRow.getCell(cellCount).setCellStyle(topBottomLeft);
-//
-//                        campaignRow.createCell(++cellCount).setCellValue(segment.getName());
-//                        //add top, bottom, right bold border
-//                        Font largerFont = wb.createFont();
-//                        largerFont.setFontHeightInPoints((short)14);
-//                        CellStyle topBottomRight = wb.createCellStyle();
-//                        topBottomRight.setFont(largerFont);
-//                        topBottomRight.setBorderLeft(CellStyle.BORDER_NONE);
-//                        topBottomRight.setBorderRight(CellStyle.BORDER_MEDIUM);
-//                        topBottomRight.setBorderTop(CellStyle.BORDER_MEDIUM);
-//                        topBottomRight.setBorderBottom(CellStyle.BORDER_MEDIUM);
-//                        campaignRow.getCell(cellCount).setCellStyle(topBottomRight);
-//
-//                        campaignRow.createCell(++cellCount).setCellValue(segment.getBoolOp());
-//                        //no borders
-//                        CellStyle noBorders = wb.createCellStyle();
-//                        noBorders.setFont(largerFont);
-//                        noBorders.setBorderLeft(CellStyle.BORDER_NONE);
-//                        noBorders.setBorderRight(CellStyle.BORDER_NONE);
-//                        noBorders.setBorderTop(CellStyle.BORDER_NONE);
-//                        noBorders.setBorderBottom(CellStyle.BORDER_NONE);
-//                        campaignRow.getCell(cellCount).setCellStyle(noBorders);
-//
-//                    }
-//                    //add condition to only display when array size > 1
-//                    campaignRow.createCell(++cellCount).setCellValue("-" + group.getBoolOp() + "-");
-//                }
-//                rowCount++;
-//            }
+            short rowCount = 1;
+            for (Campaign campaign: campaignArrayList) {
+                Row campaignRow = sheet.createRow(rowCount);
+                campaignRow.createCell(0).setCellValue(campaign.getId());
+                campaignRow.createCell(1).setCellValue(campaign.getName());
+                List<SegmentGroupTarget> currentGroupArray = campaign.getSegmentGroupTargetList();
+                short cellCount = 1;
+                for (SegmentGroupTarget group: currentGroupArray) {
+                    ArrayList<Segment> currentSegmentArray = group.getSegmentArrayList();
+                    for(Segment segment: currentSegmentArray) {
+//                        campaignRow.createCell(cellCount++)
+//                                .setCellValue("[(" + segment.getAction() + ")" + segment.getName() + "]");
+
+                        System.out.println(cellCount);
+                        if (segment.getAction().equals("exclude")) {
+                            campaignRow.createCell(++cellCount).setCellValue("(" + segment.getAction() + ")");
+                        }
+
+                        else {
+                            campaignRow.createCell(++cellCount).setCellValue("");
+                        }
+                        //add left, top, bottom bold border
+                        Font actionFont = wb.createFont();
+                        font.setFontHeightInPoints((short)14);
+                        font.setColor(HSSFColor.RED.index);
+                        CellStyle topBottomLeft = wb.createCellStyle();
+                        topBottomLeft.setFont(font);
+                        topBottomLeft.setBorderLeft(CellStyle.BORDER_MEDIUM);
+                        topBottomLeft.setBorderRight(CellStyle.BORDER_NONE);
+                        topBottomLeft.setBorderTop(CellStyle.BORDER_MEDIUM);
+                        topBottomLeft.setBorderBottom(CellStyle.BORDER_MEDIUM);
+
+                        campaignRow.getCell(cellCount).setCellStyle(topBottomLeft);
+
+                        campaignRow.createCell(++cellCount).setCellValue(segment.getName());
+                        //add top, bottom, right bold border
+                        Font largerFont = wb.createFont();
+                        largerFont.setFontHeightInPoints((short)14);
+                        CellStyle topBottomRight = wb.createCellStyle();
+                        topBottomRight.setFont(largerFont);
+                        topBottomRight.setBorderLeft(CellStyle.BORDER_NONE);
+                        topBottomRight.setBorderRight(CellStyle.BORDER_MEDIUM);
+                        topBottomRight.setBorderTop(CellStyle.BORDER_MEDIUM);
+                        topBottomRight.setBorderBottom(CellStyle.BORDER_MEDIUM);
+                        campaignRow.getCell(cellCount).setCellStyle(topBottomRight);
+
+                        campaignRow.createCell(++cellCount).setCellValue(segment.getBoolOp());
+                        //no borders
+                        CellStyle noBorders = wb.createCellStyle();
+                        noBorders.setFont(largerFont);
+                        noBorders.setBorderLeft(CellStyle.BORDER_NONE);
+                        noBorders.setBorderRight(CellStyle.BORDER_NONE);
+                        noBorders.setBorderTop(CellStyle.BORDER_NONE);
+                        noBorders.setBorderBottom(CellStyle.BORDER_NONE);
+                        campaignRow.getCell(cellCount).setCellStyle(noBorders);
+
+                    }
+                    //add condition to only display when array size > 1
+                    campaignRow.createCell(++cellCount).setCellValue("-" + group.getBoolOp() + "-");
+                }
+                rowCount++;
+            }
+
+             */
+
 
             //Repeat row for every campaign in list
             short rowCounter = 1;
@@ -134,16 +136,17 @@ public class XlsWriter {
                 }
                 campaignRow.createCell(2).setCellValue(oneLine.toString());
 
+                //Styles for patterning rows
                 CellStyle altRow = wb.createCellStyle();
-                altRow.setFillForegroundColor(HSSFColor.GREY_25_PERCENT.index);
+                altRow.setFillForegroundColor(HSSFColor.LIGHT_GREEN.index);
                 altRow.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
                 altRow.setWrapText(true);
-
                 CellStyle whiteRow = wb.createCellStyle();
                 whiteRow.setWrapText(true);
 
                 campaignRow.setHeightInPoints(linebreakCount * sheet.getDefaultRowHeightInPoints());
 
+                //Pattern rows
                 if((rowCounter % 2) == 1) {
                     campaignRow.getCell(0).setCellStyle(altRow);
                     campaignRow.getCell(1).setCellStyle(altRow);
@@ -166,7 +169,7 @@ public class XlsWriter {
 //                    3  //last column  (0-based)
 //            ));
 
-
+            //auto-size columns
             sheet.autoSizeColumn(0);
             sheet.autoSizeColumn(1);
             sheet.autoSizeColumn(2);
