9bd0d30354ef3242361c7ba5344fdee82dcf7994
KFCharron <charronkyle@gmail.com>
2013-09-09 10:54:00 -0400

    Created Campaign object
    TODO calculate daysRemaining, actualDailyBudget, totalDelivery within
    the constructor

--- src/main/java/com/mediacrossing/campaignbooks/Campaign.java
+++ src/main/java/com/mediacrossing/campaignbooks/Campaign.java
@@ -1,4 +1,26 @@
 package com.mediacrossing.campaignbooks;
 
 public class Campaign {
+    private String campaignID;
+    private String campaignName;
+    private int lifetimeBudget;
+    private String startDate;
+    private String endDate;
+    private int daysRemaining;
+    private int dailyBudget;
+    private int actualDailyBudget;
+    private int totalDelivery;
+    private int[] dailyDelivery;
+
+    public Campaign(String campaignID, String campaignName, int lifetimeBudget, String startDate, String endDate, int dailyBudget, int[] dailyDelivery) {
+        this.campaignID = campaignID;
+        this.campaignName = campaignName;
+        this.lifetimeBudget = lifetimeBudget;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.dailyBudget = dailyBudget;
+        this.dailyDelivery = dailyDelivery;
+
+        //TODO calculate daysRemaining, actualDailyBudget, totalDelivery
+    }
 }
