907951478d63f2d181e2881629fd0ba7c454a403
KFCharron <charronkyle@gmail.com>
2013-09-09 10:27:11 -0400

    Created new CampaignBook project

--- src/main/java/com/mediacrossing/campaignbooks/Run.java
+++ src/main/java/com/mediacrossing/campaignbooks/Run.java
@@ -1,4 +1,23 @@
 package com.mediacrossing.campaignbooks;
 
 public class Run {
+
+    public static void main(String[] args) {
+        //Declare variables
+
+        //Query MX for all advertisers
+
+        //Save to list of advertisers
+
+        //Query MX for line items of each advertiser
+
+        //Save line items to each advertiser
+
+        //Query AN for campaign quick stats interval=yesterday
+
+        //Save each campaign to the line item
+
+        //Build and save excel book, each sheet being its own line item
+
+    }
 }
