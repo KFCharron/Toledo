7a68d53ca471521690eb53be063589d33ee572bd
KFCharron <charronkyle@gmail.com>
2013-09-09 11:11:06 -0400

    Requests all advertisers from MX

--- src/main/java/com/mediacrossing/campaignbooks/Run.java
+++ src/main/java/com/mediacrossing/campaignbooks/Run.java
@@ -1,11 +1,19 @@
 package com.mediacrossing.campaignbooks;
 
+import com.mediacrossing.segmenttargeting.HTTPRequest;
+
 public class Run {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //Declare variables
+        String mxAdvertiserUrl = "http://ec2-50-17-18-117.compute-1.amazonaws.com:9000/api/catalog/advertisers";
+        String rawJsonData;
+        HTTPRequest httpConnection = new HTTPRequest();
 
         //Query MX for all advertisers
+        httpConnection.setUrl(mxAdvertiserUrl);
+        httpConnection.requestData();
+        rawJsonData = httpConnection.getJSONData();
 
         //Save to list of advertisers
 
