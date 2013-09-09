fe44da8d6b1be2e464928be9581bea6d5f3f9fcb
KFCharron <charronkyle@gmail.com>
2013-09-09 10:33:41 -0400

    Created LineItem object

--- src/main/java/com/mediacrossing/campaignbooks/LineItem.java
+++ src/main/java/com/mediacrossing/campaignbooks/LineItem.java
@@ -1,4 +1,22 @@
 package com.mediacrossing.campaignbooks;
 
+import java.util.List;
+
 public class LineItem {
+    private String lineItemID;
+    private String lineItemName;
+    private String startDate;
+    private String endDate;
+    private String overallBudget;
+    private String dailyBudget;
+    private List<Campaign> campaignList;
+
+    public LineItem(String lineItemID, String lineItemName, String startDate, String endDate, String overallBudget, String dailyBudget) {
+        this.lineItemID = lineItemID;
+        this.lineItemName = lineItemName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.overallBudget = overallBudget;
+        this.dailyBudget = dailyBudget;
+    }
 }
