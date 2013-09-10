575c45d652ae51a5bfefde32bc39f5824e136333
KFCharron <charronkyle@gmail.com>
2013-09-10 10:26:18 -0400

    Added LineItem header row to ExcelWriter

--- src/main/java/com/mediacrossing/campaignbooks/ExcelWriter.java
+++ src/main/java/com/mediacrossing/campaignbooks/ExcelWriter.java
@@ -17,8 +17,27 @@ public class ExcelWriter {
         Sheet lineItemSheet = WORKBOOK.createSheet(lineItem.getLineItemName());
 
         //Add line item header row
+        Row lineItemHeader = lineItemSheet.createRow(0);
+        lineItemHeader.createCell(0);
+        lineItemHeader.createCell(1).setCellValue("Line Item");
+        lineItemHeader.createCell(2).setCellValue("Lifetime Budget");
+        lineItemHeader.createCell(3).setCellValue("Start Date");
+        lineItemHeader.createCell(4).setCellValue("End Date");
+        lineItemHeader.createCell(5).setCellValue("# of Days");
+        lineItemHeader.createCell(6).setCellValue("Daily Budget");
 
         //style header
+        Font font = WORKBOOK.createFont();
+        font.setFontHeightInPoints((short) 14);
+        font.setBoldweight((short) 700);
+        CellStyle bold = WORKBOOK.createCellStyle();
+        bold.setFont(font);
+//        for(int x = 0; x < 7; x++)
+//            lineItemHeader.getCell(x).setCellStyle(bold);
+        //Try this, if fails, use commented method
+        for(Cell cell : lineItemHeader) {
+            cell.setCellStyle(bold);
+        }
 
         //add line item data
 
