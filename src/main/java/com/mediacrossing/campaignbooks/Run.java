b35a49fd8b82ca66f663452f3686cf0dba1e366b
KFCharron <charronkyle@gmail.com>
2013-09-11 14:59:03 -0400

    Removed repository logic from Run, added campaignList request

--- src/main/java/com/mediacrossing/campaignbooks/Run.java
+++ src/main/java/com/mediacrossing/campaignbooks/Run.java
@@ -1,28 +1,14 @@
 package com.mediacrossing.campaignbooks;
 
-import com.mediacrossing.campaignbooks.campaigns.CampaignRepository;
-import com.mediacrossing.campaignbooks.campaigns.PartitionedCampaignRepository;
 import com.mediacrossing.segmenttargeting.HTTPRequest;
-import scala.concurrent.duration.Duration;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class Run {
 
-    private static int APPNEXUS_PARTITION_SIZE = 10;
-    private static Duration APPNEXUS_REQUEST_DELAY = Duration.apply(60, TimeUnit.SECONDS);
-
-    private static CampaignRepository production(HTTPRequest r) {
-        return new PartitionedCampaignRepository(
-                r,
-                APPNEXUS_PARTITION_SIZE,
-                APPNEXUS_REQUEST_DELAY);
-    }
-
     public static void main(String[] args) throws Exception {
+
         //Declare variables
         String mxUrl = "http://ec2-50-17-18-117.compute-1.amazonaws.com:9000/api/catalog";
         String rawJsonData;
@@ -46,39 +32,31 @@ public class Run {
                 httpConnection.requestData();
                 rawJsonData = httpConnection.getJSONData();
                 List<LineItem> lineItemList = parser.populateLineItemList(rawJsonData);
+                for(LineItem lineItem : lineItemList) {
+                    httpConnection.setUrl(mxUrl + "/advertisers/" + advertiser.getAdvertiserID() +
+                            "/line-items" + lineItem.getLineItemID() + "/campaigns");
+                    httpConnection.requestData();
+                    rawJsonData = httpConnection.getJSONData();
+                    List<Campaign> campaignList = parser.populateCampaignList(rawJsonData);
+                    lineItem.setCampaignList(campaignList);
+                }
                 advertiser = new Advertiser(advertiser.getAdvertiserID(), lineItemList);
                 advertiserList.set(count, advertiser);
             } catch (FileNotFoundException e) {
                 advertiserList.get(count).setLive(false);
                 System.out.println(advertiser.getAdvertiserID() + ": No line items found, live set to false.");
             }
-
-
             count++;
         }
-        System.out.println(advertiserList.toString());
 
 
         //Query AN for campaign quick stats interval=yesterday
-        final CampaignRepository campaignRepository = production(httpConnection);
-
-        for(Advertiser advertiser : advertiserList) {
-            if (advertiser.isLive()) {
-                for(LineItem lineItem : advertiser.getLineItemList()) {
-                    List<String> campaignIds = new ArrayList<String>();
-                    for(Campaign campaign : lineItem.getCampaignList()) {
-                        campaignIds.add(campaign.getCampaignID());
-                    }
-                    List<Float> dailyDelivery = campaignRepository.findBy(campaignIds);
-                    //For each campaign, add corresponding float to daily delivery array
-                    int index = 0;
-                    for (Campaign campaign : lineItem.getCampaignList()) {
-                        campaign.addToDailyDeliveryList(dailyDelivery.get(index));
-                        index++;
-                    }
-                }
-            }
-        }
+        //For every advertiser, request report
+        //collect report ids
+        //check report until status is ready
+        //when ready, download report as input stream.
+        //parse csv input stream
+        //save csv vars into campaigns
 
         //Build and save excel book, each sheet being its own line item
         ExcelWriter excelWriter = new ExcelWriter();
