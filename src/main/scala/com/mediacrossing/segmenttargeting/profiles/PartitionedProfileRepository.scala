7d05461183157bf26762ef4ee9a1a81cacdc1ba3
mdiamant <michael.diamant@mediacrossing.com>
2013-09-12 09:45:01 -0400

    Introduce kludge to deal with AppNexus API rate limiting.

--- src/main/scala/com/mediacrossing/segmenttargeting/profiles/PartitionedProfileRepository.scala
+++ src/main/scala/com/mediacrossing/segmenttargeting/profiles/PartitionedProfileRepository.scala
@@ -4,10 +4,14 @@ import java.util
 import com.mediacrossing.segmenttargeting._
 import scala.collection.JavaConverters._
 import scala.concurrent.duration.Duration
+import java.io.IOException
+import org.slf4j.LoggerFactory
 
 class PartitionedProfileRepository(http: HTTPRequest,
                                    partitionSize: Int,
                                    requestDelay: Duration) extends ProfileRepository {
+
+  private val log = LoggerFactory.getLogger(classOf[HTTPRequest])
 
   def findBy(advertiserIdAndProfileIds: util.List[(String, String)]): util.List[Profile] =
     (
@@ -18,7 +22,29 @@ class PartitionedProfileRepository(http: HTTPRequest,
         for {
           (advertiserId, profileId) <- partition
         } yield {
-          http.requestProfile(profileId, advertiserId)
+          var success = false
+          var requestCount = 1
+
+          while (!success) {
+            try {
+              http.requestProfile(profileId, advertiserId)
+              success = true
+
+              log.info("Received profile [profileId = " + profileId +
+                ", advertiserId = " + advertiserId + "] after " + requestCount +
+                " request attempts")
+            } catch {
+              case e: IOException =>
+                // Rate limit exceeded
+                log.warn("Rate limit exceeded for profile [profileId = " + profileId +
+                  ", advertiserId = " + advertiserId + "], request attempts = " +
+                  requestCount)
+                requestCount = requestCount + 1
+
+                Thread.sleep(65 * 1000)
+            }
+          }
+
 
           val parser = new JSONParse
           val p = new Profile
