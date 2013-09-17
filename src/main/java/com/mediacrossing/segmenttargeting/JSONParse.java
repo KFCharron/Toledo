5537ad429c72cc2899cce107229957d906efa493
mdiamant <michael.diamant@mediacrossing.com>
2013-09-17 09:15:28 -0400

    Support parsing updated version of MX broker fees JSON.

--- src/main/java/com/mediacrossing/segmenttargeting/JSONParse.java
+++ src/main/java/com/mediacrossing/segmenttargeting/JSONParse.java
@@ -1,4 +1,5 @@
 package com.mediacrossing.segmenttargeting;
+
 import com.google.gson.*;
 
 import java.text.ParseException;
@@ -40,12 +41,12 @@ public class JSONParse {
         jobject = jobject.getAsJsonObject("response");
         jobject = jobject.getAsJsonObject("profile");
         ArrayList<DaypartTarget> newDaypartTarget = new ArrayList<DaypartTarget>();
-        if(!jobject.get("daypart_targets").isJsonNull()) {
+        if (!jobject.get("daypart_targets").isJsonNull()) {
             JsonArray karray = jobject.getAsJsonArray("daypart_targets");
-            for(int y = 0; y < karray.size(); y++) {
+            for (int y = 0; y < karray.size(); y++) {
                 JsonObject kobject = karray.get(y).getAsJsonObject();
                 //add variables to DaypartTarget
-                String day = kobject.get("day").toString().replace("\"","");
+                String day = kobject.get("day").toString().replace("\"", "");
                 int start = kobject.get("start_hour").getAsInt();
                 int end = kobject.get("end_hour").getAsInt();
                 DaypartTarget newDaypart = new DaypartTarget(day, start, end);
@@ -78,7 +79,7 @@ public class JSONParse {
         }
 
         //set country action
-        String countryAction = jobject.get("country_action").toString().replace("\"","");
+        String countryAction = jobject.get("country_action").toString().replace("\"", "");
 
         //Create new dma list
         ArrayList<DMATarget> dmaTargetList = new ArrayList<DMATarget>();
@@ -91,7 +92,7 @@ public class JSONParse {
             for (int i = 0; i < karray.size(); i++) {
                 JsonObject pobject = karray.get(i).getAsJsonObject();
                 String dma = pobject.get("dma").toString().replace("\"", "");
-                String name = pobject.get("name").toString().replace("\"","");
+                String name = pobject.get("name").toString().replace("\"", "");
                 DMATarget newDMATarget = new DMATarget(dma, name);
                 dmaTargetList.add(i, newDMATarget);
             }
@@ -99,7 +100,7 @@ public class JSONParse {
         }
 
         //set dma action
-        String dmaAction = jobject.get("dma_action").toString().replace("\"","");
+        String dmaAction = jobject.get("dma_action").toString().replace("\"", "");
 
         //Create new zip target list
         ArrayList<ZipTarget> zipTargetList = new ArrayList<ZipTarget>();
@@ -111,7 +112,7 @@ public class JSONParse {
             for (int a = 0; a < zarray.size(); a++) {
                 JsonObject zobject = zarray.get(a).getAsJsonObject();
                 String fromZip = zobject.get("from_zip").toString().replace("\"", "");
-                String toZip = zobject.get("to_zip").toString().replace("\"","");
+                String toZip = zobject.get("to_zip").toString().replace("\"", "");
                 ZipTarget newZipTarget = new ZipTarget(fromZip, toZip);
                 zipTargetList.add(a, newZipTarget);
 
@@ -128,41 +129,38 @@ public class JSONParse {
         ArrayList<Campaign> campaignArrayList1 = new ArrayList<Campaign>();
         JsonElement jelement = new JsonParser().parse(rawData);
         for (Map.Entry<String, JsonElement> entry : jelement.getAsJsonObject().entrySet()) {
-            String currentAdvertiserID = entry.getKey().replace("\"","");
+            String currentAdvertiserID = entry.getKey().replace("\"", "");
             for (JsonElement values : entry.getValue().getAsJsonArray()) {
                 Campaign newCampaign = new Campaign();
                 JsonObject jsonObject = values.getAsJsonObject();
                 newCampaign.setAdvertiserID(currentAdvertiserID);
-                newCampaign.setId(jsonObject.get("id").toString().replace("\"",""));
-                newCampaign.setState(jsonObject.get("status").toString().replace("\"",""));
-                newCampaign.setName(jsonObject.get("name").toString().replace("\"",""));
-                newCampaign.setProfileID(jsonObject.get("profileId").toString().replace("\"",""));
-                newCampaign.setLineItemID(jsonObject.get("lineItemId").toString().replace("\"",""));
-                newCampaign.setEndDate(jsonObject.get("endDate").toString().replace("\"",""));
-                if (!jsonObject.get("brokerFees").isJsonNull()) {
-                    JsonArray jarray = jsonObject.getAsJsonArray("brokerFees");
-                    ArrayList<ServingFee> newServingFeeList = new ArrayList<ServingFee>();
-                    for (int x = 0; x < jarray.size(); x++) {
-                        jsonObject = jarray.get(x).getAsJsonObject();
-                        String brokerName = jsonObject.get("broker_name").toString().replace("\"","");
-                        String paymentType = jsonObject.get("payment_type").toString().replace("\"","");
-                        String value = jsonObject.get("value").toString().replace("\"","");
-                        String description = jsonObject.get("description").toString().replace("\"","");
-                        ServingFee newServingFee = new ServingFee(brokerName, paymentType, value, description);
-                        newServingFeeList.add(x, newServingFee);
-                    }
-                    newCampaign.setServingFeeList(newServingFeeList);
+                newCampaign.setId(jsonObject.get("id").toString().replace("\"", ""));
+                newCampaign.setState(jsonObject.get("status").toString().replace("\"", ""));
+                newCampaign.setName(jsonObject.get("name").toString().replace("\"", ""));
+                newCampaign.setProfileID(jsonObject.get("profileId").toString().replace("\"", ""));
+                newCampaign.setLineItemID(jsonObject.get("lineItemId").toString().replace("\"", ""));
+                newCampaign.setEndDate(jsonObject.get("endDate").toString().replace("\"", ""));
+                JsonArray jarray = jsonObject.getAsJsonArray("brokerFees");
+                ArrayList<ServingFee> newServingFeeList = new ArrayList<ServingFee>();
+                for (int x = 0; x < jarray.size(); x++) {
+                    jsonObject = jarray.get(x).getAsJsonObject();
+                    String brokerName = jsonObject.get("name").toString().replace("\"", "");
+                    String paymentType = jsonObject.get("paymentType").toString().replace("\"", "");
+                    String value = jsonObject.get("paymentValue").toString().replace("\"", "");
+                    String description = jsonObject.get("description").toString().replace("\"", "");
+                    ServingFee newServingFee = new ServingFee(brokerName, paymentType, value, description);
+                    newServingFeeList.add(x, newServingFee);
                 }
+                newCampaign.setServingFeeList(newServingFeeList);
                 campaignArrayList1.add(newCampaign);
             }
         }
         if (campaignArrayList1.isEmpty()) {
-           LOG.error("Campaign List is empty after parse.");
-        }
-        else {
+            LOG.error("Campaign List is empty after parse.");
+        } else {
             LOG.info(campaignArrayList1.size() + "campaigns received.");
         }
-        return  campaignArrayList1;
+        return campaignArrayList1;
 
     }
 
@@ -172,23 +170,23 @@ public class JSONParse {
         jobject = jobject.getAsJsonObject("response");
         jobject = jobject.getAsJsonObject("profile");
         ArrayList<SegmentGroupTarget> newSegmentGroupTargetList = new ArrayList<SegmentGroupTarget>();
-        String segmentGroupBoolOp = jobject.get("segment_boolean_operator").toString().replace("\"","");
+        String segmentGroupBoolOp = jobject.get("segment_boolean_operator").toString().replace("\"", "");
         if (!jobject.get("segment_group_targets").isJsonNull()) {
             //Move to segment target array
             JsonArray jarray = jobject.getAsJsonArray("segment_group_targets");
             for (int x = 0; x < jarray.size(); x++) {
 
                 JsonObject jsonObject = jarray.get(x).getAsJsonObject();
-                String segmentBoolOp = jsonObject.get("boolean_operator").toString().replace("\"","");
+                String segmentBoolOp = jsonObject.get("boolean_operator").toString().replace("\"", "");
                 ArrayList<Segment> newSegmentArrayList = new ArrayList<Segment>();
-                if(!jsonObject.get("segments").isJsonNull()) {
+                if (!jsonObject.get("segments").isJsonNull()) {
                     JsonArray karray = jsonObject.getAsJsonArray("segments");
                     for (int y = 0; y < karray.size(); y++) {
 
                         JsonObject kobject = karray.get(y).getAsJsonObject();
-                        String action = kobject.get("action").toString().replace("\"","");
-                        String id = kobject.get("id").toString().replace("\"","");
-                        String name = kobject.get("name").toString().replace("\"","");
+                        String action = kobject.get("action").toString().replace("\"", "");
+                        String id = kobject.get("id").toString().replace("\"", "");
+                        String name = kobject.get("name").toString().replace("\"", "");
                         Segment newSegment = new Segment(id, name, action, segmentBoolOp);
                         newSegmentArrayList.add(y, newSegment);
                     }
@@ -200,7 +198,7 @@ public class JSONParse {
 
         }
 
-        return  newSegmentGroupTargetList;
+        return newSegmentGroupTargetList;
     }
 
 }
