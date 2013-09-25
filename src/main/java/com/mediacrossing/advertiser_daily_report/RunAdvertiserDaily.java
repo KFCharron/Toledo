b92c50ada5cc88099abed4236deabe699824cd2f
KFCharron <charronkyle@gmail.com>
2013-09-25 09:56:40 -0400

    Created new AdvertiserDailyReport run class

---
+++ src/main/java/com/mediacrossing/advertiser_daily_report/RunAdvertiserDaily.java
@@ -0,0 +1,16 @@
+package com.mediacrossing.advertiser_daily_report;
+
+public class RunAdvertiserDaily {
+    //add logging
+    //add global variables
+    //get all active advertisers
+    //request 24hr line item reports for each advertiser
+    //parse and save 24hr line item stats
+    //request lifetime line item reports for each advertiser
+    //parse and save lifetime line item stats
+    //request 24hr campaign reports for each advertiser
+    //parse and save 24hr campaign stats
+    //request lifetime campaign reports for each advertiser
+    //parse and save lifetime campaign stats
+    //generate book, each sheet being a 24hr collection of line item, 24hr of campaign, lifetime line item, life camp
+}
