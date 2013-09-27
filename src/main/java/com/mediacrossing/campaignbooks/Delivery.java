a1393b435323f24f441a8d0db6db42427bacb29b
KFCharron <charronkyle@gmail.com>
2013-09-27 09:42:53 -0400

    Added daily imps and clicks to Delivery class

--- src/main/java/com/mediacrossing/campaignbooks/Delivery.java
+++ src/main/java/com/mediacrossing/campaignbooks/Delivery.java
@@ -4,11 +4,15 @@ public class Delivery {
     private String date;
     private String campaignID;
     private float delivery;
+    private int imps;
+    private int clicks;
 
-    public Delivery(String date, String campaignID, String delivery) {
+    public Delivery(String date, String campaignID, String delivery, String imps, String clicks) {
         this.date = date;
         this.campaignID = campaignID;
         this.delivery = Float.parseFloat(delivery);
+        this.imps = Integer.parseInt(imps);
+        this.clicks = Integer.parseInt(clicks);
     }
 
     public String getDate() {
@@ -21,5 +25,13 @@ public class Delivery {
 
     public float getDelivery() {
         return delivery;
+    }
+
+    public int getImps() {
+        return imps;
+    }
+
+    public int getClicks() {
+        return clicks;
     }
 }
