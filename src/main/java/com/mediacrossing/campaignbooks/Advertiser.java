64aa0297dfdf5b56d7f931076b29078bf8b911f1
KFCharron <charronkyle@gmail.com>
2013-10-04 09:38:00 -0400

    Added DailyData lists to Advertiser class

--- src/main/java/com/mediacrossing/campaignbooks/Advertiser.java
+++ src/main/java/com/mediacrossing/campaignbooks/Advertiser.java
@@ -1,5 +1,8 @@
 package com.mediacrossing.campaignbooks;
 
+import com.mediacrossing.advertiser_daily_report.DailyData;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Advertiser {
@@ -7,6 +10,10 @@ public class Advertiser {
     private String advertiserID;
     private List<LineItem> lineItemList;
     private boolean live = true;
+    private ArrayList<DailyData> dailyLineItems;
+    private ArrayList<DailyData> dailyCampaigns;
+    private ArrayList<DailyData> lifetimeLineItems;
+    private ArrayList<DailyData> lifetimeCampaigns;
 
     public Advertiser(String advertiserID, List<LineItem> lineItemList) {
         this.advertiserID = advertiserID;
@@ -31,5 +38,37 @@ public class Advertiser {
 
     public void setLive(boolean live) {
         this.live = live;
+    }
+
+    public ArrayList<DailyData> getDailyLineItems() {
+        return dailyLineItems;
+    }
+
+    public void setDailyLineItems(ArrayList<DailyData> dailyLineItems) {
+        this.dailyLineItems = dailyLineItems;
+    }
+
+    public ArrayList<DailyData> getDailyCampaigns() {
+        return dailyCampaigns;
+    }
+
+    public void setDailyCampaigns(ArrayList<DailyData> dailyCampaigns) {
+        this.dailyCampaigns = dailyCampaigns;
+    }
+
+    public ArrayList<DailyData> getLifetimeLineItems() {
+        return lifetimeLineItems;
+    }
+
+    public void setLifetimeLineItems(ArrayList<DailyData> lifetimeLineItems) {
+        this.lifetimeLineItems = lifetimeLineItems;
+    }
+
+    public ArrayList<DailyData> getLifetimeCampaigns() {
+        return lifetimeCampaigns;
+    }
+
+    public void setLifetimeCampaigns(ArrayList<DailyData> lifetimeCampaigns) {
+        this.lifetimeCampaigns = lifetimeCampaigns;
     }
 }
