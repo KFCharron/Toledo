50629e3008a5d298ade18e5512699d58fbf93363
KFCharron <charronkyle@gmail.com>
2013-10-04 09:36:38 -0400

    created new DailyData class for AdvertiserDaily report

---
+++ src/main/java/com/mediacrossing/advertiser_daily_report/DailyData.java
@@ -0,0 +1,113 @@
+package com.mediacrossing.advertiser_daily_report;
+
+public class DailyData {
+
+    private String id;
+    private String name;
+    private String imps;
+    private String clicks;
+    private String totalConv;
+    private String mediaCost;
+    private String ctr;
+    private String convRate;
+    private String cpm;
+    private String cpc;
+    private String startDay;
+    private String dailyBudget;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImps(String imps) {
+        this.imps = imps;
+    }
+
+    public void setClicks(String clicks) {
+        this.clicks = clicks;
+    }
+
+    public void setTotalConv(String totalConv) {
+        this.totalConv = totalConv;
+    }
+
+    public void setMediaCost(String mediaCost) {
+        this.mediaCost = mediaCost;
+    }
+
+    public void setCtr(String ctr) {
+        this.ctr = ctr;
+    }
+
+    public void setConvRate(String convRate) {
+        this.convRate = convRate;
+    }
+
+    public void setCpm(String cpm) {
+        this.cpm = cpm;
+    }
+
+    public void setCpc(String cpc) {
+        this.cpc = cpc;
+    }
+
+    public void setStartDay(String startDay) {
+        this.startDay = startDay;
+    }
+
+    public void setDailyBudget(String dailyBudget) {
+        this.dailyBudget = dailyBudget;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImps() {
+        return imps;
+    }
+
+    public String getClicks() {
+        return clicks;
+    }
+
+    public String getTotalConv() {
+        return totalConv;
+    }
+
+    public String getMediaCost() {
+        return mediaCost;
+    }
+
+    public String getCtr() {
+        return ctr;
+    }
+
+    public String getConvRate() {
+        return convRate;
+    }
+
+    public String getCpm() {
+        return cpm;
+    }
+
+    public String getCpc() {
+        return cpc;
+    }
+
+    public String getStartDay() {
+        return startDay;
+    }
+
+    public String getDailyBudget() {
+        return dailyBudget;
+    }
+}
