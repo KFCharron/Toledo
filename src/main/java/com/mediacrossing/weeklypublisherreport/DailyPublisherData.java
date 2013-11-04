61bf82c6ebecc3b48d70647934b2aec3b7d73163
KFCharron <charronkyle@gmail.com>
2013-11-04 10:38:45 -0500

    Added get and post methods

--- src/main/java/com/mediacrossing/weeklypublisherreport/DailyPublisherData.java
+++ src/main/java/com/mediacrossing/weeklypublisherreport/DailyPublisherData.java
@@ -12,5 +12,59 @@ public class DailyPublisherData {
     private int unfilled;
     private float gross;
 
+    public DateTime getDate() {
+        return date;
+    }
 
+    public void setDate(DateTime date) {
+        this.date = date;
+    }
+
+    public int getAvails() {
+        return avails;
+    }
+
+    public void setAvails(int avails) {
+        this.avails = avails;
+    }
+
+    public int getImps() {
+        return imps;
+    }
+
+    public void setImps(int imps) {
+        this.imps = imps;
+    }
+
+    public int getErrors() {
+        return errors;
+    }
+
+    public void setErrors(int errors) {
+        this.errors = errors;
+    }
+
+    public float geteCpm() {
+        return eCpm;
+    }
+
+    public void seteCpm(float eCpm) {
+        this.eCpm = eCpm;
+    }
+
+    public int getUnfilled() {
+        return unfilled;
+    }
+
+    public void setUnfilled(int unfilled) {
+        this.unfilled = unfilled;
+    }
+
+    public float getGross() {
+        return gross;
+    }
+
+    public void setGross(float gross) {
+        this.gross = gross;
+    }
 }
--- src/main/java/com/mediacrossing/weeklypublisherreport/WeeklyPlacement.java
+++ src/main/java/com/mediacrossing/weeklypublisherreport/WeeklyPlacement.java
@@ -7,4 +7,22 @@ public class WeeklyPlacement {
     private String id;
     private String name;
     private ArrayList<DailyPublisherData> dailyDataList;
+
+    public WeeklyPlacement(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.dailyDataList = new ArrayList<DailyPublisherData>();
+    }
+
+    public ArrayList<DailyPublisherData> getDailyDataList() {
+        return dailyDataList;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
