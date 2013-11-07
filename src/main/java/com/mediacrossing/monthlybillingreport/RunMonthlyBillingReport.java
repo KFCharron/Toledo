aeab5e05be5ce9079aa75dbd6b8aedf5532490cc
KFCharron <charronkyle@gmail.com>
2013-11-07 10:07:58 -0500

    Completed Run class for Monthly Billing Report

---
+++ src/main/java/com/mediacrossing/monthlybillingreport/RunMonthlyBillingReport.java
@@ -0,0 +1,119 @@
+package com.mediacrossing.monthlybillingreport;
+
+import com.mediacrossing.connections.AppNexusService;
+import com.mediacrossing.connections.MxService;
+import com.mediacrossing.dailycheckupsreport.Campaign;
+import com.mediacrossing.dailycheckupsreport.ServingFee;
+import com.mediacrossing.properties.ConfigurationProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import scala.concurrent.duration.Duration;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RunMonthlyBillingReport {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RunMonthlyBillingReport.class);
+
+    public static void registerLoggerWithUncaughtExceptions() {
+        Thread.setDefaultUncaughtExceptionHandler(
+                new Thread.UncaughtExceptionHandler() {
+                    @Override
+                    public void uncaughtException(Thread t, Throwable e) {
+                        LOG.error(e.getMessage(), e);
+                    }
+                }
+        );
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        registerLoggerWithUncaughtExceptions();
+
+        //Declare variables
+        ConfigurationProperties properties = new ConfigurationProperties(args);
+        String appNexusUrl = properties.getAppNexusUrl();
+        String outputPath = properties.getOutputPath();
+        String appNexusUsername = properties.getAppNexusUsername();
+        String appNexusPassword = properties.getAppNexusPassword();
+        int anPartitionSize = properties.getPartitionSize();
+        Duration requestDelayInSeconds = properties.getRequestDelayInSeconds();
+        AppNexusService anConn = new AppNexusService(appNexusUrl, appNexusUsername,
+                appNexusPassword, anPartitionSize, requestDelayInSeconds);
+        String mxUsername = properties.getMxUsername();
+        String mxPass = properties.getMxPassword();
+        String mxUrl = properties.getMxUrl();
+        MxService mxConn = new MxService(mxUrl, mxUsername, mxPass);
+
+        //for faster debugging
+        boolean development = false;
+        if (development) {
+            try{
+                FileInputStream door = new FileInputStream("/Users/charronkyle/Desktop/ReportData/BillingList.ser");
+                ObjectInputStream reader = new ObjectInputStream(door);
+                ArrayList<BillingAdvertiser> adList = (ArrayList<BillingAdvertiser>) reader.readObject();
+                MonthlyBillingReportWriter.writeReportToFile(adList, outputPath);
+                System.exit(0);
+            }catch (IOException e){
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+
+        ArrayList<BillingAdvertiser> adList = anConn.requestBillingReport();
+        List<String[]> adExData = anConn.requestSellerReport();
+        for (String[] l : adExData) {
+            String googleAdExchangeId = "181";
+            if (l[0].equals(googleAdExchangeId)) {
+                for (BillingAdvertiser ad : adList) {
+                    for (BillingCampaign camp : ad.getCampaigns()) {
+                        if (camp.getId().equals(l[2])) {
+                            camp.setAdExImps(Integer.parseInt(l[3]));
+                        }
+                    }
+                }
+            }
+        }
+        //call on campaigns from mx
+        //parse broker fees
+        ArrayList<Campaign> feeCampaigns = mxConn.requestAllCampaigns();
+        for (Campaign c : feeCampaigns) {
+            for (BillingAdvertiser a : adList) {
+                for (BillingCampaign bc : a.getCampaigns()) {
+                    if (bc.getId().equals(c.getId())) {
+                        for (ServingFee fee : c.getServingFeeList()) {
+                            if (fee.getBrokerName().equals("Integral Ad Science")) {
+                                bc.setIntegralFee(Float.parseFloat(fee.getValue()));
+                                bc.setIntegralTotal(bc.getImps()/1000*bc.getIntegralFee());
+                            }
+                            if (fee.getBrokerName().equals("Evidon")) {
+                                bc.setEvidonFee(Float.parseFloat(fee.getValue()));
+                                bc.setEvidonTotal(bc.getImps()/1000*bc.getEvidonFee());
+                            }
+                            if (fee.getBrokerName().equals("Brilig")) {
+                                bc.setBriligFee(Float.parseFloat(fee.getValue()));
+                                bc.setBriligTotal(bc.getImps()/1000*bc.getBriligFee());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Serialize data object to a file
+        try {
+            ObjectOutputStream out = new ObjectOutputStream
+                    (new FileOutputStream("/Users/charronkyle/Desktop/ReportData/BillingList.ser"));
+            out.writeObject(adList);
+            out.close();
+        } catch (IOException e) {
+            LOG.error("Serialization Failed!");
+            LOG.error(e.toString());
+        }
+
+        MonthlyBillingReportWriter.writeReportToFile(adList, outputPath);
+
+    }
+}
